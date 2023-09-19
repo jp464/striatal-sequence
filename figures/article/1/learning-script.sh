5b47b17e7056450d99080b0685ab096d859bb9c4
JongYup Park <jp464@dcc-login-03.oit.duke.edu>
2023-09-18 22:05:21 -0400

    minor update

--- figures/article/1/learning-script.sh
+++ figures/article/1/learning-script.sh
@@ -4,14 +4,10 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem-per-cpu=4G
 #SBATCH -e slurm.err
-#SBATCH -array=0-3
+#SBATCH --array=0-3
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=$jp464@duke.edu  
 
-
-
-readarray -t FILES < learning.txt
-FILENAME=${FILES[${SLURM_ARRAY_TASK_ID}]}
-python learning.py $FILENAME
+srun $(head -n $SLURM_ARRAY_TASK_ID jobs.txt | tail -n 1)
 
 
